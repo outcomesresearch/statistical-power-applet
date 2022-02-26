fe582de156776b08673804de72ef844675a4ea6f
jeffrey <piccirilloj1@gmail.com>
2022-02-26 11:24:53 -0600

    Leverage shared code module for favicons, title of tab

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import OutcomesResearchWrapper from "shared-code";
 
-Vue.config.productionTip = false
+Vue.use(OutcomesResearchWrapper);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
