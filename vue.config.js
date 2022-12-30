15d5b6b0ea750aa09acd5be480a9d8693d74c731
jeffrey <piccirilloj1@gmail.com>
2022-12-29 23:04:20 -0600

    Add more metadata support, and a more specific description

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,20 @@
-const title = 'Statistical Power Applet'
-const description =
-  'Clinical Outcomes Research at Washington University in St Louis'
+const webpackParams = {
+  title: 'Statistical Power Applet',
+  description:
+    "Statistical Power Applet is deprecated, and we recommend checking out Universität Düsseldorf's G*Power app, available on both Mac and Windows.",
+  site_name:
+    'Applets from Clinical Outcomes Research at Washington University in St Louis',
+  url: 'https://outcomesresearch.github.io/statistical-power-applet/',
+}
 
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production' ? '/statistical-power-applet/' : '/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
-      args[0].title = title
-      args[0].description = `${title} | ${description}`
+      Object.entries(webpackParams).forEach(([key, value]) => {
+        args[0][key] = value
+      })
       return args
     })
   },
