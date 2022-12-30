12a1a9c120be5d760adb6c44bb48cd4512e8c120
jeffrey <piccirilloj1@gmail.com>
2022-12-29 22:38:16 -0600

    Rollback Vue-head changes and instead set preview metadata with webpack variables and index.html

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import OutcomesResearchWrapper from "shared-code";
-import VueHead from "vue-head";
 
 Vue.use(OutcomesResearchWrapper);
-Vue.use(VueHead);
 Vue.config.productionTip = false;
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const title = 'Statistical Power Applet'
+const description =
+  'Clinical Outcomes Research at Washington University in St Louis'
+
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/statistical-power-applet/" : "/",
-};
+    process.env.NODE_ENV === 'production' ? '/statistical-power-applet/' : '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = title
+      args[0].description = `${title} | ${description}`
+      return args
+    })
+  },
+}
