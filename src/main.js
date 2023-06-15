d4665023f259df60a459dc49b2d8bda58eedf939
jeffrey <piccirilloj1@gmail.com>
2023-06-15 18:38:43 -0500

    Add vue analytics tracking to site

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import OutcomesResearchWrapper from 'shared-code'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(OutcomesResearchWrapper)
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'G-TS2GB4BDVD',
+})
 
 new Vue({
   render: (h) => h(App),
